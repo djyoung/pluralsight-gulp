a6912fedad10c31c1b39e5989808f26da24ed5f8
Daniel Young <daniel@deviateit.net>
2015-11-12 15:55:39 +1300

    added tasks for compiling less

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,20 @@
 module.exports = function() {
+    var client = './src/client/';
+
 	var config = {
-		
+        temp: './.tmp/',
+
+        /*
+         * File paths
+         */
 		// all js to vet
 		alljs: [
 			'./src/**/*.js',
 			'./*.js'
-		]
+		],
+
+        less: client + 'styles/styles.less'
 	};
-	
+
 	return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
 	log('Analyzing source with JSHint and JSCS');
-	
+
 	return gulp
 		.src(config.alljs)
 		.pipe($.if(args.verbose, $.print()))
@@ -14,6 +15,31 @@ gulp.task('vet', function() {
 		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
 		.pipe($.jshint.reporter('fail'));
 });
+
+gulp.task('styles', ['clean-styles'], function() {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 2 versions', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function() {
+    var files = config.temp + '**/*.css';
+    clean(files);
+});
+
+gulp.task('less-watcher', function() {
+   gulp.watch([config.less], ['styles']);
+});
+
+function clean(path) {
+    log('Cleaning ' + $.util.colors.blue(path));
+    del(path);
+}
 
 function log(msg) {
 	if (typeof(msg) === 'object') {
